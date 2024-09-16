2129eda146870c3eed6b5f1aba5d8f48910d279c
Santiago <san_chan97@hotmail.com>
2024-09-16 10:48:58 -0300

    update pd

--- ej6/optiPago.cpp
+++ ej6/optiPago.cpp
@@ -7,29 +7,36 @@ using namespace std;
 int n;
 vector<int> billetes;
 int costo;
-// resolucion por backtracking
+vector<vector<tuple<int, int>>> matriz;
+// resolucion con pd
 tuple<int, int> optiPagoBT(vector<int> B, int c, int i)
 {
-    // llegue al final y no cumpli el costo
-    if (i == B.size() && c > 0)
-    {
-        return make_tuple(INF, 0);
-    }
-    // llegue al final y cumpli el costo
-    if (i == B.size() && c <= 0)
-    {
-        return make_tuple(0, 0);
-    }
     // llegue al costo en algun punto
     if (c <= 0)
     {
         return make_tuple(0, 0);
     }
+    // llegue al final
+    if (i == B.size())
+    {
+        if (c > 0)
+        {
+
+            return make_tuple(INF, 0);
+        }
+        return make_tuple(0, 0);
+    }
+
+    // si esta definido el valor, lo devuelvo
+    if (get<0>(matriz[i][c]) != -1)
+    {
+        return matriz[i][c];
+    }
     tuple<int, int> no_agrego = optiPagoBT(B, c, i + 1);
     tuple<int, int> agrego = optiPagoBT(B, c - B[i], i + 1);
     agrego = make_tuple(get<0>(agrego) + B[i], get<1>(agrego) + 1);
-    tuple<int, int> res = min(no_agrego, agrego);
-    return res;
+    matriz[i][c] = min(no_agrego, agrego);
+    return matriz[i][c];
 }
 
 int main()
@@ -37,6 +44,7 @@ int main()
     cin >> n >> costo;
     billetes.resize(n);
     int elem;
+    matriz.resize(n, vector<tuple<int, int>>(costo + 1, make_tuple(-1, 0)));
     for (int i = 0; i < n; i++)
     {
         cin >> elem;
